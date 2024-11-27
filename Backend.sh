5a86b1453e22e7231b7ee368034bc17ff21af42b
sairam <sairamkspj@gmail.com>
2024-11-27 19:34:46 +0530

    pushin data

--- Backend.sh
+++ Backend.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 LOGS_FOLDER="/var/log/expense"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
@@ -32,53 +34,52 @@ echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 CHECK_ROOT
 
-
 dnf module disable nodejs -y &>>$LOG_FILE
 VALIDATE $? "Disable default nodejs"
 
 dnf module enable nodejs:20 -y &>>$LOG_FILE
 VALIDATE $? "Enable nodejs:20"
 
-# dnf install nodejs:20 -y &>>$LOG_FILE
-# VALIDATE $? "Install nodejs"
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Install nodejs"
 
-# id expense &>>$LOG_FILE
-# if [ $? -ne 0 ]
-# then
-#     echo -e "expense user not exists... $G Creating $N"
-#     useradd expense &>>$LOG_FILE
-#     VALIDATE $? "Creating expense user"
-# else
-#     echo -e "expense user already exists...$Y SKIPPING $N"
-# fi
+id expense &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    echo -e "expense user not exists... $G Creating $N"
+    useradd expense &>>$LOG_FILE
+    VALIDATE $? "Creating expense user"
+else
+    echo -e "expense user already exists...$Y SKIPPING $N"
+fi
 
-# mkdir -p /app
-# VALIDATE $? "Creating /app folder"
+mkdir -p /app
+VALIDATE $? "Creating /app folder"
 
-# curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
-# VALIDATE $? "Downloading backend application code"
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
+VALIDATE $? "Downloading backend application code"
 
-# cd /app
-# rm -rf /app/* # remove the existing code
-# unzip /tmp/backend.zip &>>$LOG_FILE
-# VALIDATE $? "Extracting backend application code"
+cd /app
+rm -rf /app/* # remove the existing code
+unzip /tmp/backend.zip &>>$LOG_FILE
+VALIDATE $? "Extracting backend application code"
 
-# npm install &>>$LOG_FILE
-# cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
+npm install &>>$LOG_FILE
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
 
-# # load the data before running backend
+# load the data before running backend
 
-# dnf install mysql -y &>>$LOG_FILE
-# VALIDATE $? "Installing MySQL Client"
+dnf install mysql -y &>>$LOG_FILE
+VALIDATE $? "Installing MySQL Client"
 
-# mysql -h mysql.daws81s.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
-# VALIDATE $? "Schema loading"
+mysql -h mysql.daws81s.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
+VALIDATE $? "Schema loading"
 
-# systemctl daemon-reload &>>$LOG_FILE
-# VALIDATE $? "Daemon reload"
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "Daemon reload"
 
-# systemctl enable backend &>>$LOG_FILE
-# VALIDATE $? "Enabled backend"
+systemctl enable backend &>>$LOG_FILE
+VALIDATE $? "Enabled backend"
 
-# systemctl restart backend &>>$LOG_FILE
-# VALIDATE $? "Restarted Backend"
+systemctl restart backend &>>$LOG_FILE
+VALIDATE $? "Restarted Backend"
