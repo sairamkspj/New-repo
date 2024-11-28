08e5e7da8a9289e0e139174a82a5cd7a2d1cc999
sairam <sairamkspj@gmail.com>
2024-11-28 06:32:19 +0530

    adding data

---
+++ databse.sh
@@ -0,0 +1,32 @@
+Log_folder="/var/log/expense"
+Script_name=$(echo $0 | cut -d "." -f1)
+timestamp=$(date +%Y-%m-%d-%H-%M-%S)
+Log_filename="$Log_folder/$Script_name-$timestamp.log"
+mkdir -p $Log_filename
+
+userid=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+validate_root(){
+    if [ $userid -eq 1]
+    then
+        echo -e "$G user has root permissions and $Y successfull"
+    else
+        echo -e "$R user doesn't has root permissions and $Y unsuccessfull"
+}
+
+# Checking(){
+#     if [ $1 -ne 0]
+#     Then
+#         echo -e "$R $2 is $Y unsuccessful"
+#         exit 1
+#     else
+#         echo -e "$G is $Y succesfull"
+#     fi
+
+# }
+
+# dnf install mysql-server -y
+# Checking $? "install mysql-server"
